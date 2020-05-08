98d94f45bbcf0a58a7b1215fabfdeaba2a3427ef
hongyanyang1 <hongyanyang1@creditease.cn>
2020-05-08 15:30:56 +0800

    增加 vue 登录图形验证码、用户名校验

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-    <el-button @click="show = !show">Click Me</el-button>
+    <el-button @click="sendRequest">发送请求</el-button>
+    <el-button @click="login">登录</el-button>
 
     <div style="display: flex; margin-top: 20px; height: 100px;">
-      <transition name="el-fade-in-linear">
-        <div v-show="show" class="transition-box">.el-fade-in-linear</div>
-      </transition>
-      <transition name="el-fade-in">
-        <div v-show="show" class="transition-box">.el-fade-in</div>
-      </transition>
+      {{msg}}
     </div>
   </div>
 </template>
 
 <script>
   export default {
+    methods: {
+      demo: function () {
+
+      },
+      login: function () {
+        let url = this.HOST + "/login"
+        console.log("登录", url)
+        this.$post(url, {
+          "username": "admin",
+          "password": "admin1"
+        }).then(response => {
+          console.log(response);
+        }, error => {
+          //console.log(error);
+        })
+      },
+      sendRequest: function () {
+        console.log("发送请求")
+        let url = this.HOST + "/test"
+        axios.get(url, {
+          params: {
+            param: "param123123"
+          }
+        }).then(response => {
+          console.log(response.data);
+        }, error => {
+          console.log(error);
+        })
+      }
+    },
     data: () => ({
-      show: true
+      show: true,
+      msg: "消息"
     })
   }
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,15 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 
+import {get,post} from './request/http'
+
+//定义全局变量
+Vue.prototype.$get = get;
+//定义全局变量
+Vue.prototype.$post = post;
+
 Vue.use(ElementUI);
+Vue.prototype.HOST = '/api'
 
 new Vue({
   el: '#app',
